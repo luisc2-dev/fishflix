a7e3b83454d6b0d1f7a2c62cdaff65904a3df9ec
luis <luisc2@umbc.edu>
2025-04-16 02:55:27 -0400

    FIshflix First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import FishTile from "./components/FishTile";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [genre, setGenre] = useState("All");
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const matchesSearchTerm = (movie, searchTerm) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const matchesGenre = (movie, genre) => {
+    return genre === "All" || movie.genre === genre;
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      matchesSearchTerm(movie, searchTerm) && matchesGenre(movie, genre)
+  );
+
+  useEffect(() => {
+    fetch("fish_movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <title className="title">FishFlix</title>
+      <h1 className="title">FISHFLIX</h1>
+      <div className="controls">
+        <div className="controls-row">
+          <label className="genre-label">
+            Genre
+            <select value={genre} onChange={handleGenreChange}>
+              <option>All</option>
+              <option>Comedy</option>
+              <option>Adventure</option>
+              <option>Horror</option>
+            </select>
+          </label>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search Fishflix..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+      </div>
+      <div className="movie-grid">
+        {filteredMovies.map((movie) => (
+          <FishTile movie={movie} key={movie.id}></FishTile>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/FishTile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const getMovieRating = (movie) => {
+  let rating = movie.rating;
+
+  if (rating < 5) {
+    return "rating-bad";
+  }
+  if (rating >= 5 && rating < 7) {
+    return "rating-ok";
+  }
+  if (rating >= 7) {
+    return "rating-good";
+  }
+};
+
+export default function FishTile({ movie }) {
+  return (
+    <div className="movie-card" key={movie.id}>
+      <img
+        className="Image-Style"
+        src={`fish_images/${movie.image}`}
+        alt={movie.title}
+      />
+      <h1 className="movie-card-title">{movie.title}</h1>
+      <span className="movie-card-genre">{movie.genre}</span>
+      <span className={`movie-card-rating ${getMovieRating(movie)}`}>
+        {movie.rating}
+      </span>
+    </div>
+  );
+}
